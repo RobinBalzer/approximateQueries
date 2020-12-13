a918b36e3d7e7d78a3c829c636dd09328e17ac97
gurkenschurke66 <s8880157@mail.zih.tu-dresden.de>
2020-12-13 13:28:40 +0100

    typos...

--- QueryAnsweringAlgorithms/ClassicalAnswers/ProductAutomaton.java
+++ QueryAnsweringAlgorithms/ClassicalAnswers/ProductAutomaton.java
@@ -1,4 +1,5 @@
 package ClassicalAnswers;
+
 // this class is for the case where we do not search approximations. We only combine the query and the database in the productAutomaton for this.
 public class ProductAutomaton {
 }
