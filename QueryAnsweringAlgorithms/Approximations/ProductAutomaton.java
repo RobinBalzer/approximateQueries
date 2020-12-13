e9fa45463fc1037d41d05698a1033021b342f147
gurkenschurke66 <s8880157@mail.zih.tu-dresden.de>
2020-12-13 09:50:56 +0100

    minor cleanup

---
+++ QueryAnsweringAlgorithms/Approximations/ProductAutomaton.java
@@ -0,0 +1,7 @@
+package Approximations;
+
+// this is the class for the productAutomaton consisting of the three base automatons (Query, Transducer, Database) that we specified as graphs in their respective files.
+
+public class ProductAutomaton {
+
+}
